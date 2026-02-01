8625687299acf5f1983add9ca1595c23ea4a5a1a
Ilham Narendrodhipo <105351995+rilhamn@users.noreply.github.com>
2026-02-01 13:52:06 +0700

    Add files via upload

--- pages/1📊_Data_Inputation.py
+++ pages/1📊_Data_Inputation.py
@@ -20,8 +20,8 @@ LOCATION_TABLE_MAP = {
     "Well RD-I3": "sample_well_i3",
     "Well RD-N1": "sample_well_n1",
     "HP Separator": "sample_hp_separator",
-    "LP 2nd Flash Separator": "sample_lp_2nd_flash_separator"
-    "Well B1": "sample_well_b1"
+    "LP 2nd Flash Separator": "sample_lp_2nd_flash_separator",
+    "Well B1": "sample_well_b1",
 }
 
 # ===============================
@@ -35,7 +35,7 @@ location = st.selectbox(
     list(LOCATION_TABLE_MAP.keys())
 )
 
-TABLE = LOCATION_TABLE_MAP[location]
+#TABLE = LOCATION_TABLE_MAP[location]
 
 
 # ===============================
@@ -53,81 +53,81 @@ with st.form("add_sample"):
 
     submitted = st.form_submit_button("Save")
 
-    if submitted:
-        if not code_value:
-            st.error("Code value is required")
-        else:
-            try:
-                supabase.table(TABLE).insert({
-                    "code_value": code_value.strip(),
-                    "employee_name": employee_name,
-                    "department": department,
-                    "company": company
-                }).execute()
+    #if submitted:
+        #if not code_value:
+            #st.error("Code value is required")
+        #else:
+            #try:
+                #supabase.table(TABLE).insert({
+                    #"code_value": code_value.strip(),
+                    #"employee_name": employee_name,
+                    #"department": department,
+                    #"company": company
+                #}).execute()
 
-                st.success("Employee added")
-                st.rerun()
+                #st.success("Employee added")
+                #st.rerun()
 
-            except Exception as e:
-                st.error(e)
+            #except Exception as e:
+                #st.error(e)
 
 
 # ===============================
 # 📋 VIEW Sample
 # ===============================
 
-st.divider()
-st.subheader(f"📋 Sample Data list – {location}")
+#st.divider()
+#st.subheader(f"📋 Sample Data list – {location}")
 
-try:
-    res = (
-        supabase
-        .table(TABLE)
-        .select("*")
-        .order("employee_name")
-        .execute()
-    )
+#try:
+    #res = (
+        #supabase
+        #.table(TABLE)
+        #.select("*")
+        #.order("employee_name")
+        #.execute()
+    #)
 
-    df = pd.DataFrame(res.data)
+    #df = pd.DataFrame(res.data)
 
-    st.dataframe(df, use_container_width=True)
+    #st.dataframe(df, use_container_width=True)
 
-except Exception as e:
-    st.error(e)
-    df = pd.DataFrame()
+#except Exception as e:
+    #st.error(e)
+    #df = pd.DataFrame()
 
 
 # ===============================
 # 🗑 DELETE EMPLOYEE
 # ===============================
 
-st.divider()
-st.subheader(f"🗑 Delete Sample Data – {location}")
+#st.divider()
+#st.subheader(f"🗑 Delete Sample Data – {location}")
 
-if not df.empty:
+#if not df.empty:
 
-    selected_code = st.selectbox(
-        "Select employee (by code_value)",
-        df["code_value"].tolist()
-    )
+    #selected_code = st.selectbox(
+        #"Select employee (by code_value)",
+        #df["code_value"].tolist()
+    #)
 
-    col1, col2 = st.columns([1, 3])
+    #col1, col2 = st.columns([1, 3])
 
-    with col1:
-        delete_btn = st.button("Delete")
+    #with col1:
+        #delete_btn = st.button("Delete")
 
-    if delete_btn:
-        try:
-            supabase.table(TABLE)\
-                .delete()\
-                .eq("code_value", selected_code)\
-                .execute()
+    #if delete_btn:
+        #try:
+            #supabase.table(TABLE)\
+                #.delete()\
+                #.eq("code_value", selected_code)\
+                #.execute()
 
-            st.success(f"Deleted {selected_code}")
-            st.rerun()
+            #st.success(f"Deleted {selected_code}")
+            #st.rerun()
 
-        except Exception as e:
-            st.error(e)
+        #except Exception as e:
+            #st.error(e)
 
 else:
     st.info("No employee data yet.")
